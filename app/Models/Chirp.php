2ab03380cd339ec74800b5c7a2ab3a43797d42d0
sam.ebersole@groenewold-it.solutions <samuelmebersole@gmail.com>
2023-07-06 19:12:45 +0200

    Chirps now render

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
@@ -10,4 +11,9 @@ class Chirp extends Model
     use HasFactory;
 
     protected $fillable = ['message'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
